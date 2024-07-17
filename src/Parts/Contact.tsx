e6f6d0037e8c9db30515d6e6b61796bc7c652c9b
Ignas K. <ignas.kanapinskas@gmail.com>
2024-07-17 21:46:38 +0300

    Updates

--- src/Parts/Contact.tsx
+++
@@ -1,9 +0,0 @@
-const Contact = ({contactRef}: {contactRef: React.RefObject<HTMLDivElement>}) => {
-    return (
-        <div ref={contactRef} className='snap-center h-screen'>
-        <h1>Contact</h1>
-        </div>
-    );
-    }
-
-export default Contact
